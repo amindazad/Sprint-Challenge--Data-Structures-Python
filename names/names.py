f5dab40443983dbd61287a534b75e0a0bba14d6d
amindazad <amindazad@icloud.com>
2020-07-17 09:27:11 -0700

    added a binary search tree class

--- names/names.py
+++ names/names.py
@@ -1,3 +1,55 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif (target  < self.value) and (self.left is not None):
+            return self.left.contains(target)
+        elif (target > self.value) and (self.right is not None):
+            return self.right.contains(target)
+        else:
+            return False
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
+
+        # current_node = self
+        # while current_node.right is not None:
+        #     current_node = current_node.right
+        # return current_node.value
+
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        fn(self.value)
+
+        if self.left is not None:
+            self.left.for_each(fn)
+        if self.right is not None:
+            self.right.for_each(fn)
+
 import time
 
 start_time = time.time()
